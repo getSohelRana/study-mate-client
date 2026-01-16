b34bf925f34f3da3d571e9abf1f4e152817a9eb7
getSohelRana <getsohelinfo@gmail.com>
2026-01-17 00:55:25 +0600

    search not found ui experience functionality implement done

--- src/pages/FindPartners.jsx
+++ src/pages/FindPartners.jsx
@@ -5,19 +5,21 @@ import { Link, useLoaderData } from "react-router";
 const FindPartners = () => {
   const studentsInfo = useLoaderData();
   const [searchStudents, setSearchStudents] = useState([]);
-  const displayStudents =
-    searchStudents.length > 0 ? searchStudents : studentsInfo;
+  const [searchFounds, setSearcthFounds] = useState(false);
+  const displayStudents = searchFounds ? searchStudents : studentsInfo;
 
   const handleSearch = (e) => {
     e.preventDefault();
-    const search_text = e.target.search.value;
-    console.log(search_text);
+    const search_text = e.target.search.value.trim();
+    // console.log(search_text);
+    setSearcthFounds(true);
+    e.target.reset();
 
     fetch(`http://localhost:5000/search?search=${search_text}`)
       .then((res) => res.json())
       .then((data) => {
         setSearchStudents(data);
-        console.log(data);
+        // console.log(data);
       });
   };
   return (
@@ -50,7 +52,7 @@ const FindPartners = () => {
               <input
                 type="search"
                 name="search"
-                placeholder="Search profile..."
+                placeholder="Type a subject name..."
                 className="input w-full pl-10 focus:outline-0 focus:border-primary"
               />
               <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
@@ -60,46 +62,57 @@ const FindPartners = () => {
         </div>
 
         {/* ===== Cards Grid ===== */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {displayStudents.map((student) => (
-            <div
-              key={student._id}
-              className="card bg-white shadow-md hover:shadow-xl transition"
-            >
-              <div className="card-body text-center">
-                <img
-                  src={student.profileimage}
-                  alt={student.name}
-                  className="w-24 h-24 mx-auto rounded-full object-cover border-4 border-primary"
-                />
+        {/* if search matched  */}
+        {displayStudents.length > 0 && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+            {displayStudents.map((student) => (
+              <div
+                key={student._id}
+                className="card bg-white shadow-md hover:shadow-xl transition"
+              >
+                <div className="card-body text-center">
+                  <img
+                    src={student.profileimage}
+                    alt={student.name}
+                    className="w-24 h-24 mx-auto rounded-full object-cover border-4 border-primary"
+                  />
 
-                <h2 className="text-xl font-semibold mt-3">{student.name}</h2>
+                  <h2 className="text-xl font-semibold mt-3">{student.name}</h2>
 
-                <p className="text-sm text-gray-500">
-                  Subject:{" "}
-                  <span className="font-medium">{student.subject}</span>
-                </p>
+                  <p className="text-sm text-gray-500">
+                    Subject:{" "}
+                    <span className="font-medium">{student.subject}</span>
+                  </p>
 
-                <div className="flex justify-center gap-2 mt-2">
-                  <span className="badge badge-outline">
-                    {student.studyMode}
-                  </span>
-                  <span className="badge badge-secondary">
-                    {student.experienceLevel}
-                  </span>
-                </div>
+                  <div className="flex justify-center gap-2 mt-2">
+                    <span className="badge badge-outline">
+                      {student.studyMode}
+                    </span>
+                    <span className="badge badge-secondary">
+                      {student.experienceLevel}
+                    </span>
+                  </div>
 
-                <div className="card-actions justify-center mt-4">
-                  <Link
-                    to={`/student-details/${student._id}`}
-                    className="btn btn-primary btn-sm"
-                  >
-                    View Profile
-                  </Link>
+                  <div className="card-actions justify-center mt-4">
+                    <Link
+                      to={`/student-details/${student._id}`}
+                      className="btn btn-primary btn-sm"
+                    >
+                      View Profile
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        )}
+        {/* if not matched search data */}
+        <div className="flex justify-center items-center min-h-50">
+          {searchFounds && searchStudents.length === 0 && (
+            <p className="text-center text-warning text-lg flex gap-2 items-center">
+              <FaSearch></FaSearch> No students matched your search, Try a different subject name.
+            </p>
+          )}
         </div>
       </div>
     </div>
